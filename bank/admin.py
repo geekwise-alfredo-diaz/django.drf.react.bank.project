d236297f160b319beac4b197ede80910787ec5a5
FredoDiaz <alfredo.avinadiaz@gmail.com>
2019-12-12 09:55:07 -0800

    Registered: bank models

--- bank/admin.py
+++ bank/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from bank_holders.models import Branch, Account, Customer, Product
+
 
 # Register your models here.
+admin.register((
+    Branch,
+    Account,
+    Customer,
+    Product,
+))
