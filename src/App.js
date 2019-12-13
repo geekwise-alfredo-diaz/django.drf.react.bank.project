1e0839d40cb926a55f7ea2c9f467d39c3f1ec736
FredoDiaz <alfredo.avinadiaz@gmail.com>
2019-12-13 14:20:13 -0800

    Added rendering for accounts

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Modal from "./components/Modal";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeAccount: {
+        account_name: "",
+      },
+      accountsList: []
+    };
+  }
+
+  // Refresh accounts the moment component loads
+  componentDidMount() {
+    this.refreshList();
+  }
+
+  // Gets accounts from db
+  refreshList = () => {
+    axios
+      .get("https://g-f-django-bank-app.herokuapp.com/accounts/")
+      .then(res => this.setState({ accountsList: res.data }))
+      .catch(err => console.log(err));
+  };
+
+  // Renders accounts
+  renderAccounts = () => {
+    let accounts = this.state.accountsList;
+
+    return accounts.map(account => (
+      <li
+        key={account.id}
+        className="list-group-item d-flex justify-content-between align-items-center"
+      >
+        <span
+          className={`todo-title mr-2 ${
+            this.state.viewCompleted ? "completed-todo" : ""
+          }`}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          {account.name}
+        </span>
+      </li>
+    ));
+  };
+
+  render() {
+    return (
+      <main className="content">
+        <h1 className="text-white text-uppercase text-center my-4">Bank App</h1>
+        <div className="row ">
+          <div className="col-md-6 col-sm-10 mx-auto p-0">
+            <div className="card p-3">
+              <ul className="list-group list-group-flush">
+                {this.renderAccounts()}
+              </ul>
+            </div>
+          </div>
+        </div>
+      </main>
+    );
+  }
+
 }
 
 export default App;
