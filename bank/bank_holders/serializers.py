cc772a075b56fec46e2e74822cadba67fce37dab
FredoDiaz <alfredo.avinadiaz@gmail.com>
2019-12-13 14:23:10 -0800

    Added: id for fields

--- bank/bank_holders/serializers.py
+++ bank/bank_holders/serializers.py
@@ -3,20 +3,20 @@ from .models import Branch, Account, Customer, Product
 
 class Branch_Serializer(serializers.HyperlinkedModelSerializer):
     class Meta:
-        fields = ['branch_name']
+        fields = ['id', 'branch_name']
         model = Branch
 
 class Account_Serializer(serializers.HyperlinkedModelSerializer):
     class Meta:
-        fields = ['account_name']
+        fields = ['id', 'account_name']
         model = Account
 
 class Customer_Serializer(serializers.HyperlinkedModelSerializer):
     class Meta:
-        fields = ['customer_name']
+        fields = ['id', 'customer_name']
         model = Customer
 
 class Product_Serializer(serializers.HyperlinkedModelSerializer):
     class Meta:
-        fields = ['product_name']
+        fields = ['id', 'product_name']
         model = Product
