a04519fae02510191d2a12a6bac1e3704773f92b
FredoDiaz <alfredo.avinadiaz@gmail.com>
2019-12-12 16:47:55 -0800

    Added viewsets for all serializers

---
+++ bank/bank_holders/viewsets.py
@@ -0,0 +1,19 @@
+from rest_framework import viewsets
+from .serializers import Branch_Serializer, Account_Serializer, Customer_Serializer, Product_Serializer
+from .models import Branch, Account, Customer, Product
+
+class Branch_Viewset(viewsets.ModelViewSet):
+    queryset = Branch.objects.all()
+    serializer_class = Branch_Serializer
+
+class Account_Viewset(viewsets.ModelViewSet):
+    queryset = Account.objects.all()
+    serializer_class = Account_Serializer
+
+class Customer_Viewset(viewsets.ModelViewSet):
+    queryset = Customer.objects.all()
+    serializer_class = Account_Serializer
+
+class Product_Viewset(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = Product_Serializer
