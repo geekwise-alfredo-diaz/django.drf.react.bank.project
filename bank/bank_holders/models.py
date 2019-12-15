e732d2062dd8b585779fe7abff1e6896670c0f6e
FredoDiaz <alfredo.avinadiaz@gmail.com>
2019-12-15 10:26:16 -0800

    Added: defaults to all model fields

--- bank/bank_holders/models.py
+++ bank/bank_holders/models.py
@@ -1,15 +1,15 @@
 from django.db import models
 
 class Branch(models.Model):
-    name = models.CharField(max_length=30)
-    location = models.CharField(max_length=100)
+    name = models.CharField(max_length=30, default="")
+    location = models.CharField(max_length=100, default="")
 
     def __str__(self):
         return f"{self.name}: {self.location}"
 
 class Customer(models.Model):
-    name = models.CharField(max_length=30)
-    email = models.EmailField(max_length=300)
+    name = models.CharField(max_length=30, default="")
+    email = models.EmailField(max_length=300, default="")
     branch = models.ForeignKey(
         Branch,
         on_delete=models.CASCADE
@@ -19,18 +19,18 @@ class Customer(models.Model):
         return f"{self.name}: {self.branch.Branch}"
 
 class Account(models.Model):
-    name = models.CharField(max_length=30)
-    balance = models.DecimalField(max_digits=100, decimal_places=2)
+    name = models.CharField(max_length=30, default="")
+    balance = models.DecimalField(max_digits=100, decimal_places=2, default=0)
     holder = models.OneToOneField(
         Customer,
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
     )
 
     def __str__(self):
         return f"{self.name}: {self.holder}"
 
 class Product(models.Model):
-    name = models.CharField(max_length=30)
+    name = models.CharField(max_length=30, default="")
     product_options = (
         ('savings', 'SAVINGS'),
         ('checking', 'CHECKING'),
