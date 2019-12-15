19ee01f98bdca9f9b63456ab23d5f8ec21110052
FredoDiaz <alfredo.avinadiaz@gmail.com>
2019-12-15 10:10:26 -0800

    Added: more variables to models and each return more info

--- bank/bank_holders/models.py
+++ bank/bank_holders/models.py
@@ -1,21 +1,45 @@
 from django.db import models
 
 class Branch(models.Model):
-    branch_name = models.CharField(max_length=30)
-    def __str__(self):
-        return f"{self.branch_name}"
+    name = models.CharField(max_length=30)
+    location = models.CharField(max_length=100)
 
-class Account(models.Model):
-    account_name = models.CharField(max_length=30)
     def __str__(self):
-        return f"{self.account_name}"
+        return f"{self.name}: {self.location}"
 
 class Customer(models.Model):
-    customer_name = models.CharField(max_length=30)
+    name = models.CharField(max_length=30)
+    email = models.EmailField(max_length=300)
+    branch = models.ForeignKey(
+        Branch,
+        on_delete=models.CASCADE
+    )
+
     def __str__(self):
-        return f"{self.customer_name}"
+        return f"{self.name}: {self.branch.Branch}"
+
+class Account(models.Model):
+    name = models.CharField(max_length=30)
+    balance = models.DecimalField(max_digits=100, decimal_places=2)
+    holder = models.OneToOneField(
+        Customer,
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return f"{self.name}: {self.holder}"
 
 class Product(models.Model):
-    product_name = models.CharField(max_length=30)
+    name = models.CharField(max_length=30)
+    product_options = (
+        ('savings', 'SAVINGS'),
+        ('checking', 'CHECKING'),
+    )
+    product_options = models.CharField(
+        max_length=30,
+        choices=product_options,
+        default=product_options[0],
+    )
+
     def __str__(self):
-        return f"{self.product_name}"
+        return f"{self.name}: {self.product_options}"
