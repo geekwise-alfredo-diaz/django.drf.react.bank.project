05e45ae758ce2fb3ad3c1db63beffcd52d6a9b83
FredoDiaz <alfredo.avinadiaz@gmail.com>
2019-12-18 09:57:26 -0800

    Added Link routing from react

--- src/components/navigation/Header.js
+++ src/components/navigation/Header.js
@@ -1,21 +1,27 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 export default function Header() {
     return (
         <nav  className="navbar navbar-expand-lg navbar-light bg-light">
-          <a  className="navbar-brand" href="http://localhost:3001">Fredo Banks</a>
+          <Link to="/"><div className="navbar-brand">Fredo Banks</div></Link>
           <button  className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span  className="navbar-toggler-icon"></span>
           </button>
           <div  className="collapse navbar-collapse" id="navbarNavAltMarkup">
             <div  className="navbar-nav">
-              <a  className="nav-item nav-link" href="http://localhost:3001">Home <span  className="sr-only">(current)</span></a>
-              <a  className="nav-item nav-link" href="http://localhost:3001/Branches">Branches</a>
-              <a  className="nav-item nav-link" href="http://localhost:3001/Customers">Customers</a>
-              <a  className="nav-item nav-link" href="http://localhost:3001/Accounts" tabIndex="-1" aria-disabled="true">Accounts</a>
-              <a  className="nav-item nav-link" href="http://localhost:3001/Products" tabIndex="-1" aria-disabled="true">Products</a>
+              <Link to="/"><a  className="nav-item nav-link">Home <span  className="sr-only">(current)</span></a></Link>
+              <Link to="/branches"><a  className="nav-item nav-link">Branches</a></Link>
+              <Link to="/customers"><a  className="nav-item nav-link">Customers</a></Link>
+              <Link to="/accounts"><a  className="nav-item nav-link" tabIndex="-1" aria-disabled="true">Accounts</a></Link>
+              <Link to="/products"><a  className="nav-item nav-link" tabIndex="-1" aria-disabled="true">Products</a></Link>
             </div>
           </div>
         </nav>
     )
 }
+
+/*
+react-router-bootstrap
+react-bootstrap
+*/
