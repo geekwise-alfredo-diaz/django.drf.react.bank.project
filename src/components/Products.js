23eb3f2f79536d076376c39079d984d4e44237dd
FredoDiaz <alfredo.avinadiaz@gmail.com>
2019-12-18 09:53:28 -0800

    Temporary displaying branches since products wont display

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { Component } from 'react'
+import axios from 'axios'
 
-export default function Products() {
-    return (
-        <div>
-            Products
-        </div>
-    )
+export class Products extends Component {
+
+    state = {
+        productsList: [],
+    }
+
+    componentDidMount() {
+        this.refreshProducts();
+    }
+
+    // Gets products from db
+    refreshProducts = () => {
+        axios.get('https://g-f-django-bank-app.herokuapp.com/branches/')
+        .then(res => this.setState({productsList: res.data}))
+        .catch(err => console.log(err));
+    }
+
+    renderProducts = () => {
+        return this.state.productsList.map(product => (
+            <div>
+                {product.name}
+            </div>
+        ))
+    }
+
+    render() {
+        return (
+            <div>
+                {this.renderProducts()}
+            </div>
+        )
+    }
 }
+
+export default Products
