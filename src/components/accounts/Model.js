861fdf156a6e18bddb226b4f6e92d04248b44899
FredoDiaz <alfredo.avinadiaz@gmail.com>
2019-12-19 13:19:18 -0800

    Added boiler plate

---
+++ src/components/accounts/Model.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Model extends Component {
+    render() {
+        return (
+            <div>
+                {'test'}
+            </div>
+        )
+    }
+}
+
+export default Model
