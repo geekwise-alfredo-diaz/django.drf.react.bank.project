3549c70a784e043cde7c832000cc8d68f0e50719
FredoDiaz <alfredo.avinadiaz@gmail.com>
2019-12-19 15:31:08 -0800

    pushed state up with props

--- src/components/accounts/AddAccount.js
+++ src/components/accounts/AddAccount.js
@@ -2,15 +2,27 @@ import React, { Component } from 'react'
 
 export class AddAccount extends Component {
     state = {
-        name: 'Test'
+        name: ''
     }
 
+    updateName = (text)=> {
+        this.setState({name: text.target.value})
+    }
+
+    registerName = (text) => {
+        text.preventDefault();
+        this.props.addAccount(this.state.name)
+        this.setState({name: ''})
+    }
 
     render() {
         return (
-            <div>
-                {this.state.name}
-            </div>
+            <form onSubmit={this.registerName} className="input-group flex-nowrap">
+              <div className="input-group-prepend">
+                <input type="submit" className="input-group-text" id="addon-wrapping"/>
+              </div>
+              <input name="submit" onChange={this.updateName} value={this.state.name} type="text" className="form-control" placeholder="Username"/>
+            </form>
         )
     }
 }
