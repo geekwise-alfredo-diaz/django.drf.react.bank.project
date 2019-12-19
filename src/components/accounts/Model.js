6497ca281ca65391d3e79fa8029c0b1eeffdca57
FredoDiaz <alfredo.avinadiaz@gmail.com>
2019-12-19 15:32:04 -0800

    Added click event for deleting account

--- src/components/accounts/Model.js
+++ src/components/accounts/Model.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react'
 
 export class Model extends Component {
     render() {
+        const {id} = this.props.account;
         return (
             <li className="list-group-item">
                 <div>
                     {this.props.account.name}
                 </div>
-                <button className="btn-primary" onClick={this.deleteAccount}>
-                    Test
+                <button className="btn-primary" onClick={
+                    this.props.deleteAccount.bind(this, id)}>
+                    Delete
                 </button>
             </li>
         )
