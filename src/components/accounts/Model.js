dc0af563c91c856d828b2d2d34e6363c70c431b6
FredoDiaz <alfredo.avinadiaz@gmail.com>
2019-12-19 16:32:21 -0800

    Added styles for line-up

--- src/components/accounts/Model.js
+++ src/components/accounts/Model.js
@@ -4,16 +4,25 @@ export class Model extends Component {
     render() {
         const {id} = this.props.account;
         return (
-            <li className="list-group-item">
+            <li style={this.listStyle} className="list-group-item d-flex flex-row">
                 <div>
                     {this.props.account.name}
                 </div>
-                <button className="btn-primary" onClick={
+                <button style={this.buttonStyle} className="btn-primary" onClick={
                     this.props.deleteAccount.bind(this, id)}>
                     Delete
                 </button>
             </li>
         )
+    }
+
+    listStyle = {
+        position: 'relative',
+    }
+
+    buttonStyle = {
+        position: 'absolute',
+        right: '30px',
     }
 }
 
