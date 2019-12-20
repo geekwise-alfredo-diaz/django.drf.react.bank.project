49fe77757a18887fd2de947b9d531e3895ce86fb
FredoDiaz <alfredo.avinadiaz@gmail.com>
2019-12-19 19:59:16 -0800

    Changed to class component & added react bootstrap nav

--- src/components/navigation/Header.js
+++ src/components/navigation/Header.js
@@ -1,24 +1,39 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import Nav from 'react-bootstrap/Nav';
 
-export default function Header() {
-    return (
-        <nav  className="navbar navbar-expand-lg navbar-light bg-light">
-          <Link to="/"><div className="navbar-brand">Fredo Banks</div></Link>
-          <button  className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span  className="navbar-toggler-icon"></span>
-          </button>
-          <div  className="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div  className="navbar-nav">
-              <Link to="/"><div  className="nav-item nav-link">Home <span  className="sr-only">(current)</span></div></Link>
-              <Link to="/branches"><div  className="nav-item nav-link">Branches</div></Link>
-              <Link to="/customers"><div  className="nav-item nav-link">Customers</div></Link>
-              <Link to="/accounts"><div  className="nav-item nav-link">Accounts</div></Link>
-              <Link to="/products"><div  className="nav-item nav-link">Products</div></Link>
-            </div>
-          </div>
-        </nav>
-    )
+export default class Header extends React.Component {
+    linkStyle = {
+      margin: '10px 5px',
+      color: 'white',
+    }
+
+    navStyle = {
+      padding: '12px',
+      backgroundColor: '#333',
+    }
+
+    render() {
+      return (
+        <Nav style={this.navStyle} activeKey="/">
+          <Nav.Item>
+            <Link style={this.linkStyle} to="/">Home</Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Link style={this.linkStyle} to="/branches">Branches</Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Link style={this.linkStyle} to="/customers">Customers</Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Link style={this.linkStyle} to="/accounts">Accounts</Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Link style={this.linkStyle} to="/products" >Products</Link>
+          </Nav.Item>
+        </Nav>
+      )
+    }
 }
 
 /*
