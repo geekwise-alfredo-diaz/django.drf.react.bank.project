6da6b67d9924e19df56061400cb19d8238b7992d
FredoDiaz <alfredo.avinadiaz@gmail.com>
2019-12-19 20:13:41 -0800

    Added state and styles

--- src/components/navigation/Home.js
+++ src/components/navigation/Home.js
@@ -1,18 +1,31 @@
 import React, { Component } from 'react'
 
 export class Home extends Component {
+    state = {
+        know: '',
+    }
+
+    inTheKnow = {
+        marginTop: '20px',
+    }
+
     jumboStyle = {
-        height: '100vh'
+        height: '100vh',
+    }
+
+    knows = ()=> {
+        this.setState({know: 'now u know'});
     }
 
     render() {
         return (
             <div style={this.jumboStyle} className="jumbotron">
               <h1 className="display-4">Fredo Banks</h1>
-              <p className="lead">Can't lend money, but can save it for u for a at a interest rate :)</p>
+              <p className="lead">Can't lend money, but high can save it for u for a at a interest rate :)</p>
               <hr className="my-4"/>
               <p>You know, the more you know.</p>
-              <div className="btn btn-primary btn-lg" role="button">Know more</div>
+              <div onClick={this.knows} className="btn btn-primary btn-lg" role="button">Know more</div>
+              <p style={this.inTheKnow}>{this.state.know}</p>
             </div>
         )
     }
