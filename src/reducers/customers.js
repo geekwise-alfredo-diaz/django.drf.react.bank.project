f089b172eea1a2a726ca5023892d06e37cfee986
FredoDiaz <alfredo.avinadiaz@gmail.com>
2019-12-23 11:14:13 -0800

    removed payload logging to console

--- src/reducers/customers.js
+++ src/reducers/customers.js
@@ -7,7 +7,6 @@ const initialState = {
 export default function(state=initialState, action) {
     switch(action.type) {
         case GET_CUSTOMERS:
-            console.log('Payload: ' + action.payload);
             return {
                 ...state,
                 customers: action.payload,
