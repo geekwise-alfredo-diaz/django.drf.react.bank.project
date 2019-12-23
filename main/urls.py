57588f0d08e9fbf2eaeedc9bef1451cbc67f89eb
FredoDiaz <alfredo.avinadiaz@gmail.com>
2019-12-23 13:56:39 -0800

    Added: basenames

--- main/urls.py
+++ main/urls.py
@@ -5,10 +5,10 @@ from bank.bank_holders import viewsets as bank_views
 
 router = routers.DefaultRouter()
 
-router.register(r'branches', bank_views.Branch_Viewset)
-router.register(r'accounts', bank_views.Account_Viewset)
-router.register(r'customers', bank_views.Customer_Viewset)
-router.register(r'products', bank_views.Product_Viewset)
+router.register(r'branches', bank_views.Branch_Viewset, 'branches')
+router.register(r'accounts', bank_views.Account_Viewset, 'accounts')
+router.register(r'customers', bank_views.Customer_Viewset, 'customers')
+router.register(r'products', bank_views.Product_Viewset, 'products')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
