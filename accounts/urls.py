29ae5ee6c947d915c883e72e02cff1eb968f142a
FredoDiaz <alfredo.avinadiaz@gmail.com>
2019-12-24 09:55:04 -0800

    Added a viewset for login

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
-from .veiwsets import Register_Viewset
+from .veiwsets import Register_Viewset, Login_Viewset
 from knox import views as knox_views
 
 urlpatterns = [
     path('auth', include('knox.urls')),
     path('auth/register', Register_Viewset.as_view()),
+    path('auth/login', Login_Viewset.as_view()),
 ]
