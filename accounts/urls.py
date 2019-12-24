a1b85c614f9ac33d0119bc471bb9a3022d589fbb
FredoDiaz <alfredo.avinadiaz@gmail.com>
2019-12-23 17:06:41 -0800

    BugFix: mispelled urlpatterns

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from .veiwsets import Register_Viewset
 from knox import views as knox_views
 
-urlpatters = [
+urlpatterns = [
     path('/auth', include('knox.urls')),
     path('/auth/register', Register_Viewset.as_view()),
 ]
