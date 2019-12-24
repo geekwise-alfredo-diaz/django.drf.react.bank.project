e3f47c613c18cf0f7ce217aa671908a54f168407
FredoDiaz <alfredo.avinadiaz@gmail.com>
2019-12-23 17:22:16 -0800

    removed beginning trailing slash for debugging

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,6 +3,6 @@ from .veiwsets import Register_Viewset
 from knox import views as knox_views
 
 urlpatterns = [
-    path('/auth', include('knox.urls')),
-    path('/auth/register', Register_Viewset.as_view()),
+    path('auth', include('knox.urls')),
+    path('auth/register', Register_Viewset.as_view()),
 ]
