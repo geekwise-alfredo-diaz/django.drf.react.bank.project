305288aada11a75e4f97fbe1a63ff534a926bb61
FredoDiaz <alfredo.avinadiaz@gmail.com>
2019-12-24 10:29:00 -0800

    Added custom auth urls before knox

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,7 +3,7 @@ from .veiwsets import Register_Viewset, Login_Viewset
 from knox import views as knox_views
 
 urlpatterns = [
-    path('auth', include('knox.urls')),
     path('auth/register', Register_Viewset.as_view()),
     path('auth/login', Login_Viewset.as_view()),
+    path('auth', include('knox.urls')),
 ]
