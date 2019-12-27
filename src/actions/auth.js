7eb227ed6b0f0f971c807bdbdf3438bc20b99f50
FredoDiaz <alfredo.avinadiaz@gmail.com>
2019-12-26 16:49:37 -0800

    Debug: headers wasn't meant to be caps

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,19 +1,17 @@
 import axios from 'axios';
-import { USER_LOADED, USER_LOADING, AUTH_ERROR } from './types'
+import { USER_LOADED, USER_LOADING, AUTH_ERROR, LOGIN_FAIL, LOGIN_SUCCESS } from './types'
 
 export const loadUser = () => (dispatch, getState) => {
     dispatch({type: USER_LOADING});
-
     const token = getState().auth.token;
-
     const config = {
-        Headers: {
+        headers: {
             'Content-Type': 'application/json',
         }
     }
 
     if(token) {
-        config.Headers['Authorization'] = `Token ${token}`
+        config.headers['Authorization'] = `Token ${token}`
     }
 
     axios.get('https://g-f-django-bank-app.herokuapp.com/auth/user', config)
@@ -21,8 +19,31 @@ export const loadUser = () => (dispatch, getState) => {
         dispatch({
             type: USER_LOADED,
             payload: res.data,
-        })})
+        })
+        // Temporary for continous sign in 
+        dispatch({type: AUTH_ERROR});
+    })
+        
     .catch(err => {
         dispatch({type: AUTH_ERROR});
+    })
+}
+
+export const login = (username, password) => dispatch => {
+    const config = {
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    }
+    const body = JSON.stringify({username, password});
+
+    axios.post('https://g-f-django-bank-app.herokuapp.com/auth/login', body, config)
+    .then(res => {
+        dispatch({
+            type: LOGIN_SUCCESS,
+            payload: res.data,
+        })})
+    .catch(err => {
+        dispatch({type: LOGIN_FAIL});
     })
 }
