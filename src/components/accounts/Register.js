bf607ee0cc5965c165cf948218998a685c4e250a
FredoDiaz <alfredo.avinadiaz@gmail.com>
2019-12-26 19:41:12 -0800

    Added fields for user

--- src/components/accounts/Register.js
+++ src/components/accounts/Register.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
 import { Form, Button } from 'react-bootstrap';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { register } from '../../actions/auth';
+import { Link, Redirect } from 'react-router-dom';
 
 export class Register extends Component {
 
     state = {
+        username: '',
         email: '',
         password: '',
+    }
+
+    static propTypes = {
+      register: PropTypes.func.isRequired,
+      isAuthenticated: PropTypes.bool,
+    }
+
+    nameInput = (nameText) => {
+        this.setState({username: nameText.target.value});
     }
 
     emailInput = (emailText) => {
@@ -18,19 +32,29 @@ export class Register extends Component {
 
     formSubmit = (e) => {
         e.preventDefault();
-        console.log(this.state.email);
-        console.log(this.state.password);
+        const {email, password, username} = this.state
+        const newUser = {username, email, password};
+        this.props.register(newUser);
         this.setState({email: ''});
         this.setState({password: ''});
+        this.setState({username: ''});
     }
 
 
     render() {
+        console.log('Auth: ' + this.props.isAuthenticated);
+        if(this.props.isAuthenticated) {
+          return <Redirect to="/"/>
+        }
 
-        let {email, password} = this.state;
+        const {username, email, password} = this.state;
 
         return (
             <Form onSubmit={this.formSubmit} style={this.FormStyle} className="container">
+              <Form.Group controlId="formBasicName">
+                <Form.Label>Username</Form.Label>
+                <Form.Control onChange={this.nameInput} value={username} type="username" placeholder="Enter name" />
+              </Form.Group>
               <Form.Group controlId="formBasicEmail">
                 <Form.Label>Email address</Form.Label>
                 <Form.Control onChange={this.emailInput} value={email} type="email" placeholder="Enter email" />
@@ -54,4 +78,8 @@ export class Register extends Component {
     }
 }
 
-export default Register
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect(mapStateToProps, {register})(Register)
