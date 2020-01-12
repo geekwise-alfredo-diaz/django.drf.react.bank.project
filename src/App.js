badd9113adaaa9dbeebad2b5014cb700870da019
FredoDiaz <geekwise.alfredo.diaz@gmail.com>
2020-01-12 15:07:56 -0800

    Moved routes to `AppRoutes.js`

--- src/App.js
+++ src/App.js
@@ -1,70 +1,29 @@
+// Native Imports
 import React, { Component } from "react";
-import { HashRouter as Router, Route } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 
 // Navigation
-import Home from './components/navigation/Home';
 import Header from './components/navigation/Header';
-import SideBar from './components/navigation/SideBar';
-
-// Components
-import Branches from './components/Branches';
-import Accounts from './components/Accounts';
-import Customers from './components/Customers';
-import Products from './components/Products';
-
-import Members from './components/Members'
-
-// Authentication
-import Login from './components/accounts/Login';
-import Register from './components/accounts/Register'
-import PrivateRoute from './components/navigation/PrivateRoute';
-
-// Stage Management
-import { Provider } from 'react-redux';
-import store from './store';
-import { loadUser } from './actions/auth';
+import AppRoutes from './components/AppRoutes'
 
 // Context
-import BranchContextProvider from './context/BranchProvider'
-import MemberContextProvider from './context/MemberProvider'
+import AuthContextProvider from './context/AuthProvider'
+
 
 class App extends Component {
-  componentDidMount() {
-    store.dispatch(loadUser());
-  }
 
   render() {
     return (
-      <Provider store={store}>
+      // Provides context for auth
+      <AuthContextProvider>
         <Router>
 
           <Header />
 
-          <div className="d-flex flex-row">
-            <Route path="/" component={SideBar}/>
-
-
-            <Route exact path="/" component={Home}/>
-
-            {/*Branches*/}
-            <BranchContextProvider>
-              <Route path="/branches" component={Branches}/>
-            </BranchContextProvider>
-
-            <Route path="/accounts" component={Accounts}/>
-            {/* Private Route */}
-
-            <MemberContextProvider>
-              <PrivateRoute path="/holders" component={Members}/>
-            </MemberContextProvider>
-
-            <Route path="/products" component={Products}/>
-            <Route path="/login" component={Login}/>
-            <Route path="/register" component={Register}/>
-          </div>
+          <AppRoutes />
 
         </Router>
-      </Provider>
+      </AuthContextProvider>
     );
   }
 }
