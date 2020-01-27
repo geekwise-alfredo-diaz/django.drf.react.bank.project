057a3a11cb20481581053d10dc9cc90d50d24a16
FredoDiaz <geekwise.alfredo.diaz@gmail.com>
2020-01-27 08:32:35 -0800

    Changed customer to members

--- bank/bank_holders/viewsets.py
+++ bank/bank_holders/viewsets.py
@@ -1,20 +1,28 @@
 from rest_framework import viewsets, permissions
-from .serializers import Branch_Serializer, Account_Serializer, Customer_Serializer, Product_Serializer
-from .models import Branch, Account, Customer, Product
+from .serializers import Branch_Serializer, Account_Serializer, Member_Serializer, Product_Serializer
+from .models import Branch, Account, Member, Product
 
 class Branch_Viewset(viewsets.ModelViewSet):
     queryset = Branch.objects.all()
     serializer_class = Branch_Serializer
 
 class Account_Viewset(viewsets.ModelViewSet):
-    queryset = Account.objects.all()
-    serializer_class = Account_Serializer
-
-class Customer_Viewset(viewsets.ModelViewSet):
     permission_classes = [
         permissions.IsAuthenticated,
     ]
-    serializer_class = Customer_Serializer
+    serializer_class = Account_Serializer
+
+    def get_queryset(self):
+        return self.request.user.holders.all()
+
+    def perform_create(self, serializer):
+        serializer.save(holder=self.request.user)
+
+class Member_Viewset(viewsets.ModelViewSet):
+    permission_classes = [
+        permissions.IsAuthenticated,
+    ]
+    serializer_class = Member_Serializer
 
     def get_queryset(self):
         return self.request.user.customers.all()
