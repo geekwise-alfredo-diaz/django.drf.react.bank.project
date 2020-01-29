17f153ab824da377066b0f6855648955b30eabdd
FredoDiaz <geekwise.alfredo.diaz@gmail.com>
2020-01-29 10:01:42 -0800

    Added: actions

---
+++ src/actions/accounts.js
@@ -0,0 +1,35 @@
+import axios from 'axios';
+
+export const deleteAccount = (accountId) => dispatch => {
+    axios.delete(`https://g-f-django-bank-app.herokuapp.com/accounts/${accountId}/`)
+    .then(res => {})
+}
+
+export const addAccount = (submitText) => dispatch => {
+    axios.post('https://g-f-django-bank-app.herokuapp.com/accounts/',
+    {
+        name: submitText
+    }).then(res => {})
+    .catch(err => console.log(err));
+}
+
+export const updateAccount = (accountId, accountName) => dispatch => {
+    let body = {
+      id: accountId,
+      name: accountName
+  }
+
+  axios.put(`https://g-f-django-bank-app.herokuapp.com/accounts/${accountId}/`, body)
+  .then(res => {})
+  .catch(err => console.log(err));
+}
+
+export const refreshAccounts = () => dispatch => {
+    axios.get('https://g-f-django-bank-app.herokuapp.com/accounts/')
+    .then(res => 
+        dispatch({
+            type: 'RENDER_ACCOUNTS',
+            payload: res.data
+        }))
+    .catch(err => console.log(err))
+}
