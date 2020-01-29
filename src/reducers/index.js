f05e7d1d8211f7d3d97f332671d10c5dcbaf2097
FredoDiaz <geekwise.alfredo.diaz@gmail.com>
2020-01-29 10:02:48 -0800

    Added: accounts

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,10 +4,12 @@ import customers from './customers';
 import auth from './auth';
 import branches from './branches'
 import staff from './staffReducer'
+import accounts from './accounts'
 
 export default combineReducers({
     customers,
     auth,
     branches,
     staff,
+    accounts,
 });
