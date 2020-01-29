4064644f55d37e467db0d556197e3b22db4c2519
FredoDiaz <geekwise.alfredo.diaz@gmail.com>
2020-01-29 10:04:45 -0800

    Removed: comments

--- src/reducers/branches.js
+++ src/reducers/branches.js
@@ -1,21 +1,3 @@
-// const branchReducer = (state, action) => {
-//     switch(action.type){
-//         case 'RENDER_BRANCHES':
-//             return [...action.payload]
-//         case 'ADD_BRANCH':
-//             return [action.payload, ...state]
-//         case 'DELETE_BRANCH':
-//             return [...state.filter(
-//                 branch => branch.id !== action.payload)]
-//         default:
-//             return state;
-
-//     }
-// }
-
-// export default branchReducer;
-
-
 const initialState = {
     branches: [],
 }
